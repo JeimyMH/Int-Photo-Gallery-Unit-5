6e2b7ad44eee94938e51c6d8eeb5536d09738903
Jeimy Mora Hidalgo <164965643+JeimyMH@users.noreply.github.com>
2025-01-27 17:14:53 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+/* my own JS for search box, photo gallary project*/
+
+const search= document. getElementById ("search");
+let photos = document.querySelectorAll("a");
+search.addEventListener("keyup", e => {}
+    let currentValue = e.target.value.toLowerCase();
+    photos.forEach(photo => {
+        if (getAttribute(data-caption).textContent.toLowerCase()
+        .includes(currentValue)) {
+            photo.parentNode.parentNode.style.display = "block";
+        } else {
+            photo.parentNode.parentNode.style.display = "none";
+        }
+    });
+
+
+
+
+
+
+baguetteBox.run('.gallery');
